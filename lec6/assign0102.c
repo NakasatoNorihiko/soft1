ea317b4ac167cfdd00a2cccb9d4325cb8d05a751
Norihiko Nakasato <n.norihiko1229@gmail.com>
2014-11-13 15:44:41 +0900

    assign02.txt, assign0102の改良

--- lec6/assign0102.c
+++ lec6/assign0102.c
@@ -1,4 +1,4 @@
-/* 宿題１２で指定された動作を行うプログラム */
+/* 宿題１,２で指定された動作を行うプログラム */
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXN 100
@@ -10,7 +10,7 @@ int main()
     const double x0 = 0.1;
     int i;
 
-    for (i = 0; i < MAXN; i++){
+    for (i = 0; i <= MAXN; i++){
         printf("N: %2d x:%.20lf\n", i, pie(x0, i));
     }
     return 0;
